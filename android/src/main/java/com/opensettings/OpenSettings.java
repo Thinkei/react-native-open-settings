8d89d05edd456f0479a71acb55a71b45c87b9c1e
Tan Nguyen <tan.nguyen@employmenthero.com>
2018-08-21 12:15:30 +0700

    fix no activity found to handle intent

--- android/src/main/java/com/opensettings/OpenSettings.java
+++ android/src/main/java/com/opensettings/OpenSettings.java
@@ -40,11 +40,7 @@ public class OpenSettings extends ReactContextBaseJavaModule {
     public void openSystemSettings() {
         final Intent i = new Intent();
         i.setAction(Settings.ACTION_SETTINGS);
-        i.addCategory(Intent.CATEGORY_DEFAULT);
-        i.setData(Uri.parse("package:" + reactContext.getPackageName()));
         i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-        i.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
         reactContext.startActivity(i);
     }
     //endregion
